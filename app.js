cf52301e470c043bf3daf1a96f15ff8b6d9756ea
Tutanrhamun <katsuki.hanawa.job@gmail.com>
2016-12-28 16:56:38 +0900

    for pages

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var utils = require('./twit_utils');
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(process.env.PORT || 8000);
+console.log('Server running');
 
 utils.get_screen_name()
   .then(utils.streaming);
